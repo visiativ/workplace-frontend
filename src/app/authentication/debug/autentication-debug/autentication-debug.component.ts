6b9a1e00567ccf1b424225027b11b1ef5502e479
famaridon <famaridon@users.noreply.github.com>
2019-08-28 16:33:19 +0200

    pretty print identity claims

--- src/app/authentication/debug/autentication-debug/autentication-debug.component.ts
+++ src/app/authentication/debug/autentication-debug/autentication-debug.component.ts
@@ -1,5 +1,7 @@
+import { map } from 'rxjs/operators';
 import { AuthenticationService } from './../../authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-autentication-debug',
@@ -10,8 +12,12 @@ export class AutenticationDebugComponent implements OnInit {
 
   public authenticationService: AuthenticationService;
 
+  public identityClaimsPretty$: Observable<string>;
+
   constructor(authenticationService: AuthenticationService) {
     this.authenticationService = authenticationService;
+    this.identityClaimsPretty$ = this.authenticationService.identityClaims$
+      .pipe(map(claim => JSON.stringify(claim, null, 2)));
   }
 
   ngOnInit() {
